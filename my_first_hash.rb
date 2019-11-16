209a66f0d0c5d59ba98a14a74f890ce720853e38
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-16 20:54:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,14 +18,14 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
+  # shipping_manifest = {
+  #                 "whale bone corsets" => 5,
+  #                 "porcelain vases" => 2,
+  #                 "oil paintings" => 3
+  #                }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  hash["oil paintings"]
+  hash["key2"]
 end
 
 def adding
