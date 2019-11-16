7ccfdd375c5744af012ee00f7c6996d359145875
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-16 21:03:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,9 +25,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  hash["key"]
 end
-hash["key3"]
 
 def adding
   shipping_manifest = {
